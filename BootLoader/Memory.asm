0d457355d14919ca62cfe7e5e7a065e3739d0f37
khaira nabila <75076265+khairanabila@users.noreply.github.com>
2022-09-20 19:13:18 +0700

    fix: restructure code

--- BootLoader/Memory.asm
+++
@@ -1,8 +0,0 @@
-[bits 16]
-[cpu 286]
-MemoryAvailable: dw 0
-GetMemoryAvailable:
-  mov ax, 0
-  int 0x12
-  mov [MemoryAvailable], ax
-  ret
--- Bootloader/Print.asm
+++ Bootloader/Print.asm
@@ -1,23 +1,33 @@
 [bits 16]
 [cpu 286]
-
 PrintString:
   push ax
-  mov ah, 0x0e
-  
+  mov ah, 0x0e 
+
   .Loop:
-    lodsb
+    lodsb 
+
     cmp al, byte 0
     je .Exit
+
     int 0x10
+
     jmp .Loop
+
+  .Exit:
+    pop ax
+    ret
 
 PrintNewLine:
   push ax
   mov ah, 0x0e
   mov al, 10
   int 0x10
+
   mov al, 13
   int 0x10
+
   pop ax
+
   ret
+
